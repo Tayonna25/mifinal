fa40f6c41be8f1b43531c5af01d6ce11046e30a2
Tayonna25 <113479181+Tayonna25@users.noreply.github.com>
2023-04-30 23:15:06 -0400

    tt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function Dashboard() {
+  const [calendarData, setCalendarData] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    // Fetch calendar data
+    fetch('https://api.calendar.com/events')
+      .then(response => response.json())
+      .then(data => setCalendarData(data));
+
+    // Fetch weather data
+    fetch('https://api.weather.com/forecast')
+      .then(response => response.json())
+      .then(data => setWeatherData(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with TayTay at msu
-        </a>
-      </header>
+    <div>
+      <h1>My Fitness Dashboard</h1>
+      {calendarData && (
+        <div>
+          <h2>Upcoming Events</h2>
+          <ul>
+            {calendarData.map(event => (
+              <li key={event.id}>{event.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      {weatherData && (
+        <div>
+          <h2>Current Weather</h2>
+          <p>{weatherData.current.temperature}°F</p>
+          <p>{weatherData.current.conditions}</p>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default Dashboard;
+
+ 
